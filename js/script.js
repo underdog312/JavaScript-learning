c5a6330e8fae3b8976d11e7ccdd162aed2d66baf
Ievgenii Sotnykov <ievgeniis@wix.com>
2024-06-18 08:49:10 +0300

    add homework

---
+++ js/script.js
@@ -0,0 +1,114 @@
+// TASK 1
+
+// let country = "Ukraine";
+// let city = "Kyiv";
+// let address = "Hmyri str.";
+
+// let fullAdress = `${country}, ${city}, ${address}`;
+
+// console.log(fullAdress);
+
+// TASK 2
+
+// let width = 25;
+// let height = 15;
+
+// let area = width * height;
+
+// console.log(area);
+
+// TASK 3
+
+// let result = 17 ** 6;
+// console.log(result);
+
+// TASK 4
+
+// let random = Math.ceil(Math.random() * 70);
+// console.log(random);
+
+// TASK 5
+
+// let number = 10;
+
+// if (number % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// number % 2 === 0 ? console.log("even") : console.log("odd");
+
+// TASK 6
+
+// let text = "Hello, world $";
+
+// if (text.includes("$")) {
+//   text = text.replace("$", "€");
+// }
+
+// console.log(text);
+
+// TASK 7
+
+// let monthNumber = 10;
+
+// switch (monthNumber) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+//   case 6:
+//     console.log("June");
+//     break;
+//   case 7:
+//     console.log("July");
+//     break;
+//   case 8:
+//     console.log("August");
+//     break;
+//   case 9:
+//     console.log("September");
+//     break;
+//   case 10:
+//     console.log("October");
+//     break;
+//   case 11:
+//     console.log("November");
+//     break;
+//   case 12:
+//     console.log("December");
+//     break;
+//   default:
+//     console.log("wrong month number");
+// }
+
+// TASK 8
+
+// let password = "&asbabs#avv";
+// let pass = password.trim();
+
+// if (pass.length > 10 && pass.includes("&") && pass.includes("#")) {
+//   console.log("You have strong password");
+// } else {
+//   console.log("Please, try another password");
+// }
+
+// TASK 9
+// let a = 8;
+// let b = 25;
+
+// typeof a === "number" && typeof b === "number"
+//   ? console.log(a + b)
+//   : console.log("Sum of those items can’t be counted");
